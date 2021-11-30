71e1bc0220f070e82567e11e51c1751acba73528
deepak2810 <deepakkhanka.97@gmail.com>
2021-11-30 14:42:23 +0530

    first commit

---
+++ Day 04/nested.py
@@ -0,0 +1,13 @@
+# practice of nested loop
+
+""" 
+program to print multiplication table(2-10) using nested for loop.
+
+"""
+
+#outer loop
+for i in range(2,11):
+    for j in range(1,11):
+        print(i*j,end=" ")
+
+    print()    
