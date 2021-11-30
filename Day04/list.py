fd07fe9832ea274041ff03eed55101881b133c9a
deepak2810 <deepakkhanka.97@gmail.com>
2021-11-30 19:31:27 +0530

    updated

---
+++ Day04/list.py
@@ -0,0 +1,10 @@
+"""
+program for list
+
+"""
+# simple list to display players name of a cricket team
+ls=["deepak","mayank","asish","manish","vikas","sanjay","ajay","rahul","raju","bhamdari","Manoj"]
+print(ls)
+
+#To print a particular index in python
+print(ls[1])
