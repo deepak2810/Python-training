d849b2be23d1b6c030fb28f575f9888b9d1a1b3a
deepak2810 <deepakkhanka.97@gmail.com>
2021-12-08 12:54:51 +0530

    updated code

---
+++ Day08/pck2.py
@@ -0,0 +1,7 @@
+# python program for finding factorial of a program using math packages.
+
+import math
+
+d = math.factorial(4)
+
+print(d)
---
+++ Day08/pkg1.py
@@ -0,0 +1,8 @@
+"""
+A simple python program to demonstrate about python packages.
+"""
+
+# python program to print value of pie using math package.
+import math
+
+print(math.pi)
