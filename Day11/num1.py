e107ec6869f1bcbabec5003177939a6a7786578b
deepak2810 <deepakkhanka.97@gmail.com>
2021-12-30 14:43:27 +0530

    numpy programs

---
+++ Day11/num1.py
@@ -0,0 +1,11 @@
+# #  A simple python program to demonstrate about numpy.
+# # Numpy is a python library used for working with array.
+import numpy as np
+a=np.array([1,2,3,4,5])
+print(a)
+print(a[0],a[1],a[2])
+
+# import sys
+# print(sys.path)
+
+
