06d561a1e100bf0597a4fea18078d3527000cba4
deepak2810 <deepakkhanka.97@gmail.com>
2022-01-01 11:51:19 +0530

    random one dimensional array code

---
+++ Day11/randarray.py
@@ -0,0 +1,9 @@
+# python program to print an array with random integer.
+
+import numpy as np
+
+d = np.random.randint((2,8,(3,2)))
+
+print(d)
+
+
---
+++ Day11/randarray2.py
@@ -0,0 +1,8 @@
+# python program to create a random array using np.random function
+
+import numpy as np
+
+a= np.random.randint(2,size=4)
+
+print(a)
+
