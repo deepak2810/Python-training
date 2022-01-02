96c0853868dfede0dcc3ee9b96aebb0eb112c5c2
deepak2810 <deepakkhanka.97@gmail.com>
2022-01-02 19:26:27 +0530

    prep

---
+++ Day13/conditional.py
@@ -0,0 +1,12 @@
+# python revision for technical interview preperation.
+
+# python program to demonstrate about conditional statements.
+
+a= int(input('please enter a positive number.'))
+if a%2==0:
+    print('number is a even number.')
+
+else:
+    print('number is a odd number.')
+
+    
