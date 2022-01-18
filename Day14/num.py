5d1f3a533a769c972241e9e6f28202a07bfac763
deepak2810 <deepakkhanka.97@gmail.com>
2022-01-18 12:51:11 +0530

    today code

---
+++ Day14/num.py
@@ -0,0 +1,19 @@
+import  numpy as np
+"""
+a = np.array([1,2,3])
+
+print(a)
+
+"""
+
+# element wise addition of two arrays
+
+c = np.array([[10,20,32],[10,12,15]])
+d = np.array([[25,2,15],[11,42,35]])
+
+a = c+d
+
+print("addition of both arrays is: ", end=" ")
+print(a)
+
+
