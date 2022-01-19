cbdf2b0264ad5905d330027a034869deb5d69146
deepak2810 <deepakkhanka.97@gmail.com>
2022-01-19 23:50:59 +0530

    dict code

---
+++ Day14/dict1.py
@@ -0,0 +1,11 @@
+# Using dictionary
+
+dict = {"1":"Deepak","2":"Anurag","3":"Mohit","4":"Priyanshu"}
+
+#to print keys of dictionary
+print(dict.keys())
+
+#to print all the attrubutes associated with dictionary in python.
+print(dir(dict))
+
+
