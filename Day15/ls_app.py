a98652ffc416f439e32172618b683230bd1e7df1
deepak2810 <deepakkhanka.97@gmail.com>
2022-01-20 19:48:12 +0530

    list append code

---
+++ Day15/ls_app.py
@@ -0,0 +1,15 @@
+"""
+python program to demonstrate about list clear and append Attribute
+
+"""
+
+#create a list 
+
+ls = [10,20,30,40]
+#print(dir(list))
+
+
+# to add an element at the end of the list. we use list.append method.
+ls.append("hlw")
+
+print(ls)
