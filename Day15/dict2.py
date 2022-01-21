ae7dcae4ec918549d4b0abae2482dd40b9072199
deepak2810 <deepakkhanka.97@gmail.com>
2022-01-21 21:58:15 +0530

    dictionay in python

---
+++ Day15/dict2.py
@@ -0,0 +1,12 @@
+# creating a dictionary.
+
+tut = {"aarav":1200,"Priyanshu":500,"mohit":1400}
+
+# to access values from dictionary.
+print("Pending fee for next month is  : ",end=" ")
+print(tut["aarav"]+tut["mohit"]+tut["Priyanshu"])
+
+# print(tut("Priyanshu"))
+
+
+# print(tut("aarav"))
